20ff9c80c55395b2399c89a1579d8250d175eed4
Fatima20027 <fatima.hailou@icloud.com>
2024-10-28 19:49:03 +0100

    task 1

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,19 @@
+import $ from "jquery";
+
+$(document).ready(() => {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  const button = $('<button>Click here to get started</button>');
+  $('body').append(button);
+  $('body').append("<p id='count'></p>");
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  function updateCounter(){
+    const count = 0;
+    count += 1;
+        $('#count').text(`${count} clicks on the button`); 
+  }
+
+  button.on('click', debounce(updateCounter, 300));
+
+});
