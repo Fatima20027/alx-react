152ff29749b7f64a21d80034484a96d491649906
Fatima20027 <fatima.hailou@icloud.com>
2024-11-13 00:18:25 +0100

    task 2

--- 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -1,64 +1,70 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { StyleSheet, css } from "aphrodite";
+import React, { Fragment }from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 
+function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+	const styleRow = { backgroundColor: '#f5f5f5ab' };
+	const styleHeaderRow = { backgroundColor: '#deb5b545' };
 
-const row = {
-  backgroundColor: "#f5f5f5ab",
-};
+	let myElement;
+	if (isHeader === true) {
+		if (textSecondCell === null) {
+			myElement = <th colSpan="2" className={css(styles.headerRow)}>{textFirstCell}</th>;
+		} else {
+			myElement = (
+				<Fragment>
+					<th className={css(styles.defaultRow)}>{textFirstCell}</th>
+					<th className={css(styles.defaultRow)}>{textSecondCell}</th>
+				</Fragment>
+			);
+		}
+	} else {
+		myElement = (
+			<Fragment>
+				<td>{textFirstCell}</td>
+				<td>{textSecondCell}</td>
+			</Fragment>
+		);
+	}
 
-const headerRow = {
-  backgroundColor: "#deb5b545"
+	let stylesBackground;
+
+	if (isHeader) {
+		stylesBackground = styleHeaderRow;
+	} else {
+		stylesBackground = styleRow;
+	}
+	return (
+		<tr style={stylesBackground}>{myElement}</tr>
+	);
 }
 
 const styles = StyleSheet.create({
-  defaultRows:{
-    fontfamily: 'Arial, Helvetica, sans-serif',
+	headerRow: {
+		fontfamily: 'Arial, Helvetica, sans-serif',
+		textAlign: 'center'
+	},
+
+	defaultRow: {
+		fontfamily: 'Arial, Helvetica, sans-serif',
 		borderbottom: '1px solid lightgray',
 		height: '25px',
 		textAlign: 'left'
-
-  },
-
-  headerRows:{
-    fontfamily: 'Arial, Helvetica, sans-serif',
-		textAlign: 'center'
-    
-
-  },
-})
-
-function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
-  return (
-    <tr style={row}>
-      {isHeader ? (
-        textSecondCell === null ? (
-          <th colSpan={2} className={css(styles.headerRows)}>{textFirstCell}</th>
-        ) : (
-          <>
-            <th className={css(styles.headerRows)}>{textFirstCell}</th>
-            <th className={css(styles.headerRows)}>{textSecondCell}</th>
-          </>
-        )
-      ) : (
-        <>
-          <td style={css(styles.defaultRows)}>{textFirstCell}</td>
-          <td style={css(styles.defaultRows)}>{textSecondCell}</td>
-        </>
-      )}
-    </tr>
-  );
-}
+	}
+});
 
 CourseListRow.propTypes = {
-  isHeader: PropTypes.bool,
-  textFirstCell: PropTypes.string.isRequired,
-  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+	isHeader: PropTypes.bool,
+	textFirstCell: PropTypes.string.isRequired,
+	textSecondCell: PropTypes.oneOfType([
+		PropTypes.string,
+		PropTypes.number
+	])
 };
 
 CourseListRow.defaultProps = {
-  isHeader: false,
-  textSecondCell: null,
+	isHeader: false,
+	textSecondCell: null
 };
 
 export default CourseListRow;
