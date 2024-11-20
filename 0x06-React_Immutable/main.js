c5cc58e0a200d8c7864d65eb74cf5e5793061607
Fatima20027 <fatima.hailou@icloud.com>
2024-11-20 21:13:53 +0100

    task 1

--- 0x06-React_Immutable/main.js
+++
@@ -1,6 +0,0 @@
-import { getImmutableObject } from './0-fromjs';
-
-const obj = { name: "Tsuki", age: 25, hobbies: ["coding", "reading"] };
-const immutableMap = getImmutableObject(obj);
-
-console.log(immutableMap); // Outputs an Immutable.js Map
